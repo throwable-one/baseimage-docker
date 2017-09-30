f8cb4c798101808aa876177503472c637284900e
ilya.kazakevich <ilya.kazakevich@jetbrains.com>
2017-09-30 05:30:09 +0300

    Locale added

--- image/prepare.sh
+++ image/prepare.sh
@@ -39,4 +39,4 @@ $minimal_apt_get_install software-properties-common
 
 ## Upgrade all packages.
 apt-get dist-upgrade -y --no-install-recommends -o Dpkg::Options::="--force-confold"
-
+apt-get install locales
