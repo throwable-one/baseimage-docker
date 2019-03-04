2ec3a931273b4afe489d79d4458c47bf517adb86
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2019-03-04 21:51:06 +0300

    install locales

--- image/prepare.sh
+++ image/prepare.sh
@@ -39,6 +39,6 @@ $minimal_apt_get_install software-properties-common
 
 ## Upgrade all packages.
 apt-get dist-upgrade -y --no-install-recommends -o Dpkg::Options::="--force-confold"
-apt-get install locales
+apt-get install locales -y
 
 
